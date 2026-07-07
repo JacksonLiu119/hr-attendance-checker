efef0459683e46d9230e208ff22d3b61f55e1056
Jackson Liu <jacksonliu119@gmail.com>
2026-07-07 10:24:35 +0800

    修正匯入 11506-B.xlsx 與下載檔名問題，並新增啟動腳本與安全防護

---
+++ 啟動系統.bat
@@ -0,0 +1,15 @@
+@echo off
+chcp 65001 > nul
+echo 正在啟動 仙暉 HR 考勤月結檢核系統本地伺服器...
+echo.
+echo ==================================================
+echo   伺服器已啟動！請勿關閉本視窗。
+echo   使用完畢後，直接關閉本視窗即可。
+echo ==================================================
+echo.
+
+:: 在瀏覽器中開啟 localhost 網址
+start "" "http://localhost:8888/index.html"
+
+:: 啟動 Python 本地網頁伺服器
+python -m http.server 8888
